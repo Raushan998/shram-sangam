4945407956ed211fc201b0b806f5faba3d1ceb04
Raushan Kumar Raman <your.email@example.com>
2025-03-29 19:01:03 +0530

    added new contractor for user interface

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -8,7 +8,8 @@ class Worker < ApplicationRecord
         building_contractor: 5, 
         painter: 6, 
         civil_contractor: 7,
-        tile_contractor: 8
+        tile_contractor: 8,
+        painting_contractor: 9
     }
     def self.ransackable_attributes(auth_object = nil)
         ["addr1", "addr2", "city", "country", "country_code", "created_at", "description", "email", "id", "id_value", "name", "phone_number", "pincode", "state", "updated_at", "work_type"]
